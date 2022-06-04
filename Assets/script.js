73f6c5028ee4a03b5a71068aa2495b19ab04b28a
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-04 13:33:38 +0300

    provides conditions for log in if and else

--- Assets/script.js
+++ Assets/script.js
@@ -10,3 +10,33 @@ const verifyPassword = document.getElementById("verify-password");
 
 
 let accountArray = []
+logIn.addEventListener("click", function () {
+    if(inputName.value.length<=5){
+        verifyName.textContent = "Invalid Name*";
+        verifyName.style.color = "red"
+    }
+    else{
+        verifyName.textContent = "Valid Name";
+        verifyName.style.color = "green"
+    }
+
+
+    if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(inputEmail.value)){
+        verifyEmail.textContent = "valid Email";
+        verifyEmail.style.color = "green";
+    }
+    else{
+        verifyEmail.textContent = "this is required*";
+        verifyEmail.style.color = "red";
+    }
+
+    if(inputPassword.value.length<=8){
+        verifyPassword.textContent = "Too Short*";
+        verifyPassword.style.color = "red"
+    }
+    else{
+        verifyPassword.textContent = "Valid Password";
+        verifyPassword.style.color = "green"
+        location.href = "index.html";
+    }
+})
