44b8424174f7cde2de0cd4f4ddf3a5ce5eb43027
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-04 13:28:12 +0300

    modifies js to have constants

---
+++ Assets/script.js
@@ -0,0 +1,12 @@
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const inputPassword = document.getElementById("password");
+const logIn = document.getElementById("log-in");
+const signUp = document.getElementById("sign-up");
+const accountData = document.getElementById("list");
+const verifyName = document.getElementById("verify-name");
+const verifyEmail = document.getElementById("verify-email");
+const verifyPassword = document.getElementById("verify-password");
+
+
+let accountArray = []
